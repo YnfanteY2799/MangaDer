c8df07435b7fb4f7568f678dd4dde12569ea7770
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2022-05-10 12:49:41 -0400

    +Adding Search Page

---
+++ src/pages/Search/Search.tsx
@@ -0,0 +1,6 @@
+export default function Search(){
+
+
+
+     return <>Buscar</>;
+}
--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -1,13 +1,15 @@
-import { RoutingObject, RouteChildren, NavbarRoute } from "../types/types";
+import { RoutingObject, RouteChildren } from "../types/types";
 import Home from "../pages/Home/Home";
 import Login from "./Login/Login";
 import Settings from "./Settings/Settings";
 import IndexingHome from "./Home/IndexingHome";
+import Search from "./Search/Search";
 
 export const HomeChildrends: Array<RouteChildren> = [
   { path: "Login", Element: Login },
   { path: "Settings", Element: Settings },
   { path: "Home", Element: IndexingHome, include: false },
+  { path: "Search", Element: Search },
 ];
 
 const PagesList: RoutingObject[] = [
